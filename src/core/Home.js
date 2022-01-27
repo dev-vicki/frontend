928ad977278f1cf7932686e5c723030e91795fb5
dev-vicki <vignesh.r1999@gmail.com>
2022-01-27 15:12:23 +0530

    connecting frontend to backend with env variables

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { API } from "../backend";
 
 export default function Home() {
+  console.log("API IS", API);
   return <div>
       <h1>hello frontend</h1>
   </div>;
