5f727e809a63ff2419197e3755fc09178fcac0da
dev-vicki <vignesh.r1999@gmail.com>
2022-01-27 16:38:21 +0530

    signup and signin Nav works

---
+++ src/user/Signup.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+import Base from "../core/Base";
+import {Link} from "react-router-dom";
+
+const Signup = () => {
+    return (
+      <Base title="Sign up page" description="Signed up successfully">
+        <h1>Sign up works</h1>
+      </Base>
+    );
+}
+
+export default Signup;
