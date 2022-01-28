416e8c4b12ec228b054a1a384ef4dcd70c628f65
dev-vicki <vignesh.r1999@gmail.com>
2022-01-28 12:24:34 +0530

    fixed: footer stand-alone.

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -3,9 +3,33 @@ import Base from "../core/Base";
 import { Link } from "react-router-dom";
 
 const Signin = () => {
+
+    const signInForm = () => {
+      return (
+        <div className="row">
+          <div className="col-md-6 0ffset-sm-3 text-left">
+            <form>
+              <div className="form-group">
+                <label className="text-light">Email</label>
+                <input className="form-control" type="email" />
+              </div>
+
+              <div className="form-group">
+                <label className="text-light">Password</label>
+                <input className="form-control" type="password" />
+              </div>
+              <button className="btn btn-success btn-block mt-auto">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      );
+    };
+
   return (
     <Base title="Sign in page" description="Signed in successfully">
-      <h1>Sign in works</h1>
+     {signInForm()}
     </Base>
   );
 };
--- src/user/Signup.js
+++ src/user/Signup.js
@@ -3,9 +3,38 @@ import Base from "../core/Base";
 import {Link} from "react-router-dom";
 
 const Signup = () => {
+
+        const signUpForm = () => {
+            return (
+              <div className="row">
+                <div className="col-md-6 0ffset-sm-3 text-left">
+                  <form>
+                    <div className="form-group">
+                      <label className="text-light">Name</label>
+                      <input className="form-control" type="text" />
+                    </div>
+
+                    <div className="form-group">
+                      <label className="text-light">Email</label>
+                      <input className="form-control" type="email" />
+                    </div>
+
+                    <div className="form-group">
+                      <label className="text-light">Password</label>
+                      <input className="form-control" type="password" />
+                    </div>
+                    <button className="btn btn-success btn-block mt-auto">
+                      Submit
+                    </button>
+                  </form>
+                </div>
+              </div>
+            );
+        }
+
     return (
       <Base title="Sign up page" description="Signed up successfully">
-        <h1>Sign up works</h1>
+        {signUpForm()}
       </Base>
     );
 }
